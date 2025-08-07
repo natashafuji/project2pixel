8e258d457e80b80c5e0c8977247595cfa0172cd9
natashafuji <natashafredericks24@gmail.com>
2025-08-07 15:19:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const navLinks = document.querySelector(".nav-links");
-
-  // Only attach event if elements exist
   hamburger?.addEventListener("click", function () {
     navLinks?.classList.toggle("show");
   });
-
   const header = document.querySelector("header");
   window.addEventListener("scroll", function () {
     if (window.scrollY > 50) {

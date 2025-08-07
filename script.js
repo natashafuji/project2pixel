fe0b29ac72a56a7f7e6426c9617ce58c62cc686e
natashafuji <natashafredericks24@gmail.com>
2025-08-07 14:46:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
+  const hamburger = document.getElementById("hamburger");
   const navLinks = document.querySelector(".nav-links");
 
-  if (hamburger && navLinks) {
-    hamburger.addEventListener("click", function () {
-      navLinks.classList.toggle("show");
-    });
-  }
+  hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+  });
 
   const header = document.querySelector("header");
   window.addEventListener("scroll", function () {

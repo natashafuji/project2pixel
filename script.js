03e0d7ae30dcacc426ff53656980eddd358fc92c
natashafuji <natashafredericks24@gmail.com>
2025-08-07 13:54:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
     navLinks.classList.toggle("show");
   });
 });
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+});
+

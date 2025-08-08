100380138f4ff042711508a28a91172dd80de4af
natashafuji <natashafredericks24@gmail.com>
2025-08-08 14:58:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,11 +19,5 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Hamburger toggle
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
 
